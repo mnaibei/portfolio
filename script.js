6144c359498bf94e6700fdbb279afa8501dbd6ce
mnaibei <muchajulius@gmail.com>
2023-03-06 19:12:31 +0300

    fix linter errors

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", function(){
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
 
-document.querySelectorAll(".nav-menu").forEach(n => n.addEventListener("click", function () {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+document.querySelectorAll('.nav-menu').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
